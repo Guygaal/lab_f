d3833c5d4c2634c8b5f321e64a3a8f4c4f84bf99
InkieNorfleet <spectr7757@gmail.com>
2019-01-19 21:36:09 +0300

    Added files and changes required for deployment to heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -51,6 +52,7 @@ CKEDITOR_UPLOAD_PATH = "media/post_images/"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,3 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 BOOTSTRAP3 = {
     'include_jquery': True,
     }
+# Heroku: Update database configuration from $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
